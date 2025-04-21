22f795cb548a17ef20193fe941ccb4ab545e9c03
Josh Park <84344359+joshpark8@users.noreply.github.com>
2025-04-20 22:32:16 -0400

    finish finish

--- app.py
+++ app.py
@@ -173,6 +173,7 @@ app.layout = html.Div(
                     "margin": "0",
                     "padding": "0",
                 },
+                config={"responsive": True},
             ),
             style={"width": "100%", "display": "block", "margin": "0", "padding": "0"},
         ),
